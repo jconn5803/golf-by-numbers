4a15d5d9c8dfaa3fcb25974093bf4f678bbff1b9
jconn5803 <jconn5803@hotmail.com>
2025-03-31 00:13:44 +0100

    remove config

--- config.py
+++ config.py
@@ -19,8 +19,7 @@ class ProductionConfig(Config):
     # For example, for PostgreSQL: "postgresql://user:password@host:port/dbname"
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     
-    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
 
    
 
