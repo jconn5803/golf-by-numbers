db92309f563fecd579a98a52bfb3be1689c5f359
jconn5803 <jconn5803@hotmail.com>
2025-03-31 23:23:04 +0100

    config

--- config.py
+++ config.py
@@ -18,7 +18,9 @@ class ProductionConfig(Config):
     # Get the production database URI from an environment variable.
     # For example, for PostgreSQL: "postgresql://user:password@host:port/dbname"
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-    
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
 
 
    
