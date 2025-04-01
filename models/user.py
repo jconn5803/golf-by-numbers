c67261456ccc6c91a96adcac743ef48497cc5a5e
jconn5803 <jconn5803@hotmail.com>
2025-04-01 21:09:05 +0100

    Updated password hash lenght

--- models/user.py
+++ models/user.py
@@ -8,7 +8,7 @@ class User(UserMixin, db.Model):
     userID = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     first_name = db.Column(db.String(128), nullable = False)
     last_name = db.Column(db.String(128), nullable = False)
 
