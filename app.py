d8ccc66a6c5af071cc82a706555dcf1cbe844980
jconn5803 <jconn5803@hotmail.com>
2025-04-13 12:49:24 +0100

    added in a pricing page

--- app.py
+++ app.py
@@ -244,6 +244,10 @@ def handle_checkout_session(session):
 def index():
     return render_template('index.html')
 
+@app.route('/pricing')
+def pricing():
+    return render_template('pricing.html')
+
 
 # Route for user signup
 @app.route('/register', methods=['GET', 'POST'])
