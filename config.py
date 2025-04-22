2d6c20c5a6820af0e31abf54a5228fdd3b6ccdc2
jconn5803 <jconn5803@hotmail.com>
2025-04-22 21:00:02 +0100

    stripe keys made in all configs

--- config.py
+++ config.py
@@ -7,18 +7,19 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False  # Default to False
-
-class DevelopmentConfig(Config):
-    DEBUG = True  # Enable debug mode for development
-    # Use SQLite for development
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'instance/app.db')}"
-    # Get the test Stripe keys
     STRIPE_PUBLISHABLE_KEY = os.getenv("STRIPE_PUBLISHABLE_KEY")
     STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
     STRIPE_ENDPOINT_SECRET=os.getenv("STRIPE_ENDPOINT_SECRET")
     STRIPE_MONTHLY_PRICE_ID=os.getenv("STRIPE_MONTHLY_PRICE_ID")
     STRIPE_ANNUAL_PRICE_ID=os.getenv("STRIPE_ANNUAL_PRICE_ID")
     STRIPE_DAILY_PRICE_ID=os.getenv("STRIPE_DAILY_PRICE_ID")
+
+class DevelopmentConfig(Config):
+    DEBUG = True  # Enable debug mode for development
+    # Use SQLite for development
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'instance/app.db')}"
+    # Get the test Stripe keys
+    
 
 class ProductionConfig(Config):
     DEBUG = False  # Ensure debug is off in production
